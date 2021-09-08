1c9c4d4834c43667209f34286a3b605cbd169cda
harshparab <harsh_parab@outlook.com>
2021-09-08 10:03:06 +0530

    getting error

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
-const billAmount = document.querySelector("#bill-amount")
-const cashGiven = document.querySelector("#cash-given")
-const checkButton = document.querySelector("#check-button")
-const message = document.querySelector(".error-message")
+const billAmount = document.querySelector("#bill-amount");
+const cashGiven = document.querySelector("cash-given");
+const checkButton = document.querySelector("#check-button");
+const message = document.querySelector("#error-message")
 
-checkButton.addEventListener("clicked", function validateBillAmountAndCashGiven(){
-    if (billAmount > 0 ){
-
-    }else {
-        MessageChannel.innerText = "The bill Amout should be greater than zero"
-     }
+checkButton.addEventListener("click", function validateBillAmountAndCashGiven() {
+    hideMessage()
+    if (billAmount.value > 0) {
+        if(cashGiven.value >= billAmount.value){
+            const amountToBeReturned = cashGiven.value - billAmount.value
+            calculateChange(amountToBeReturned)
+        }else {
+            showMessage("The cash given must be atleast equal to the bill amount.")
+        }
+    } else {
+        showMessage("Invalid Bill Amount")
+    }
 })
+
+function hideMessage() {
+    message.style.display = "none";
+}
+
+function showMessage(msg) {
+    msg.style.display = "block";
+        msg.innerHTML = msg
+}
