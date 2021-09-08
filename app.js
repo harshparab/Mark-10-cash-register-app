fb17e735fad106a1973c54b7192428aa3c9fc3f2
harshparab <harsh_parab@outlook.com>
2021-09-08 18:05:02 +0530

    css remaining

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const billAmount = document.querySelector("#bill-amount");
-const cashGiven = document.querySelector("cash-given");
+const cashGiven = document.querySelector("#cash-given");
 const checkButton = document.querySelector("#check-button");
 const message = document.querySelector("#error-message")
+const noOfNotes = document.querySelectorAll(".no-of-notes")
+
+const availableNotes = [2000, 500, 100, 20, 10, 5, 1]
 
 checkButton.addEventListener("click", function validateBillAmountAndCashGiven() {
     hideMessage()
     if (billAmount.value > 0) {
-        if(cashGiven.value >= billAmount.value){
+        if (cashGiven.value >= billAmount.value) {
             const amountToBeReturned = cashGiven.value - billAmount.value
             calculateChange(amountToBeReturned)
-        }else {
+        } else {
             showMessage("The cash given must be atleast equal to the bill amount.")
         }
     } else {
@@ -17,11 +20,22 @@ checkButton.addEventListener("click", function validateBillAmountAndCashGiven() 
     }
 })
 
+function calculateChange(amountToBeReturned) {
+    for (let i = 0; i < availableNotes.length; i++) {
+        const numberOfNotes = Math.trunc(
+            amountToBeReturned / availableNotes[i]
+        )
+            amountToBeReturned %= availableNotes[i]
+            noOfNotes[i].innerHTML = numberOfNotes
+
+    }
+}
+
 function hideMessage() {
     message.style.display = "none";
 }
 
 function showMessage(msg) {
-    msg.style.display = "block";
-        msg.innerHTML = msg
+    message.style.display = "block"
+    message.innerHTML = msg
 }
